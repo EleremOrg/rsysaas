e6c7a4ce9c9a654e8f46a3c8d74a4fb93f2e7179
Lucas <lluc23@hotmail.com>
2024-10-12 23:21:15 +0200

    add dashboard

--- src/dashboard/views.rs
+++ src/dashboard/views.rs
@@ -7,6 +7,8 @@ use super::seo::Meta;
 pub fn routes(state: AppState) -> Router<AppState> {
     Router::new()
         .route("/", get(index))
+        .route("/login", get(login))
+        .route("/register", get(register))
         .route("/import-data", get(import_data))
         .with_state(state)
 }
@@ -29,6 +31,40 @@ async fn index() -> IndexTemplate {
     IndexTemplate { meta }
 }
 
+#[derive(Template)]
+#[template(path = "auth/login.html")]
+pub struct LoginTemplate {
+    meta: Meta,
+}
+
+async fn login() -> LoginTemplate {
+    let meta = Meta::new(
+        "import data".into(),
+        "elerem mola".into(),
+        "recsys,mola".into(),
+        "lucas montes".into(),
+        "elerem.com".into(),
+        "imafge.com".into(),
+    );
+   LoginTemplate { meta }
+}
+#[derive(Template)]
+#[template(path = "auth/register.html")]
+pub struct RegisterTemplate {
+    meta: Meta,
+}
+
+async fn register() -> RegisterTemplate {
+    let meta = Meta::new(
+        "import data".into(),
+        "elerem mola".into(),
+        "recsys,mola".into(),
+        "lucas montes".into(),
+        "elerem.com".into(),
+        "imafge.com".into(),
+    );
+    RegisterTemplate { meta }
+}
 #[derive(Template)]
 #[template(path = "import_data.html")]
 pub struct BaseTemplate {
